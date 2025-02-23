80030c7216e59c4df2fbc4e86a117c2bfdd059ef
vlad <oskin0425@gmail.com>
2025-02-23 22:10:01 +0400

    add fsd

---
+++ src/features/pagination/ui/PaginationButtons/PaginationButtons.tsx
@@ -0,0 +1,49 @@
+import { useTheme } from "@/app/providers/ThemeProvider";
+import { IPaginationProps } from "../../model/types";
+import styles from "./styles.module.css";
+
+const PaginationButtons = ({
+  totalPages,
+  handlePageClick,
+  handlePrevPage,
+  handleNextPage,
+  currentPage,
+}: IPaginationProps) => {
+  const { isDark } = useTheme();
+  return (
+    <div
+      className={`${styles.pagination} ${isDark ? styles.dark : styles.light}`}
+    >
+      <button
+        disabled={currentPage <= 1}
+        onClick={handlePrevPage}
+        className={styles.arrow}
+      >
+        {"<"}
+      </button>
+      <div className={styles.list}>
+        {[...Array(totalPages)].map((_, i) => {
+          return (
+            <button
+              onClick={() => handlePageClick(i + 1)}
+              className={styles.pageNumber}
+              disabled={i + 1 === currentPage}
+              key={i}
+            >
+              {i + 1}
+            </button>
+          );
+        })}
+      </div>
+      <button
+        disabled={currentPage >= totalPages}
+        onClick={handleNextPage}
+        className={styles.arrow}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+export default PaginationButtons;
